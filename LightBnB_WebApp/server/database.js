ef9ed5ec0bfdc44894902ca91e30a1a5259d6860
Claire Devlin <claire.devlin@hotmail.com>
2021-05-27 15:44:01 -0400

    Implement addProperty function with SQL queries

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -154,8 +154,8 @@ const getAllProperties = function (options, limit = 10) {
   `;
 
   // 5
-  console.log("queryString", queryString);
-  console.log("queryParams", queryParams);
+  // console.log("queryString", queryString);
+  // console.log("queryParams", queryParams);
 
   // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
@@ -169,9 +169,47 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+  // 2
+  let queryString = `
+  INSERT INTO properties(owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `;
+
+  console.log("queryString", queryString);
+  console.log("queryParams", queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
+  // INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *;
 }
 exports.addProperty = addProperty;
